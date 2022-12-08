464dc31f35aa31467e3175816c2ecc28015ca6fa
bmiakoun <bmiakoun@gmail.com>
2022-12-08 17:00:02 +0800

    remove meta class

--- store/models.py
+++ store/models.py
@@ -42,12 +42,6 @@ class Customer(models.Model):
   membership = models.CharField(
     max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
   )
-  
-  class Meta:
-    db_table = 'store_customers'
-    indexes = [
-      models.Index(fields=['last_name', 'first_name']),
-    ]
 
 
 class Order(models.Model):
